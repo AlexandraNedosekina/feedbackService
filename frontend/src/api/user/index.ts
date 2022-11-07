fa45ee9b42ca71cf4b0073bc2192b2facf213b29
Vlad Gafarov <vlad.gafarov02@gmail.com>
2022-11-07 23:08:17 +0500

    add endpoints for avatar

--- frontend/src/api/user/index.ts
+++ frontend/src/api/user/index.ts
@@ -1,2 +1,4 @@
 export { default as createAvatar } from './createAvatar'
+export { default as getAvatarThumbnail } from './getAvatarThumbnail'
+export { default as getOriginalAvatar } from './getOriginalAvatar'
 export { default as getUser } from './getUser'
