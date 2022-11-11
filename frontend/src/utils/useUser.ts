2e928411854c368d7be9c25284f85c4230219561
Vlad Gafarov <vlad.gafarov02@gmail.com>
2022-11-11 15:21:44 +0500

    update useUser hook

--- frontend/src/utils/useUser.ts
+++ frontend/src/utils/useUser.ts
@@ -2,15 +2,16 @@ import { useQuery } from '@tanstack/react-query'
 import { getUser, QueryKeys } from 'src/api'
 import { User } from 'src/api/generatedTypes'
 
-export const useUser = (): User => {
-	const { data } = useQuery({
+export const useUser = () => {
+	const { data, isLoading, isError, isFetching } = useQuery({
 		queryKey: [QueryKeys.USER],
 		queryFn: getUser,
 	})
 
-	if (!data) {
-		throw new Error('User not found')
+	return {
+		user: data as User,
+		isLoading,
+		isError,
+		isFetching,
 	}
-
-	return data
 }
