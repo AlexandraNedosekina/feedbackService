06eb0deb5ed4a0d5320378bbbddb395ec52b8bf3
Vlad Gafarov <vlad.gafarov02@gmail.com>
2022-11-18 09:37:07 +0500

    add loader while checking refresh-token cookie

--- frontend/src/layouts/BaseLayout.tsx
+++ frontend/src/layouts/BaseLayout.tsx
@@ -1,17 +1,29 @@
 import AppShell from '@components/AppShell'
+import { LoadingOverlay } from '@mantine/core'
 import { getCookie } from 'cookies-next'
 import { useRouter } from 'next/router'
-import { ReactNode, useEffect } from 'react'
+import { ReactNode, useEffect, useState } from 'react'
 
 const BaseLayout = ({ children }: { children: ReactNode }) => {
 	const router = useRouter()
 	const cookie = getCookie('refresh-token')
+	const [isLoading, setIsLoading] = useState(true)
 
 	useEffect(() => {
 		if (!cookie) {
 			router.push('/')
 		}
+
+		setIsLoading(false)
 	}, [cookie, router])
+
+	if (isLoading || !cookie) {
+		return (
+			<div style={{ position: 'relative', width: '100vw', height: '100vh' }}>
+				<LoadingOverlay visible={true} />
+			</div>
+		)
+	}
 
 	return <AppShell>{children}</AppShell>
 }
