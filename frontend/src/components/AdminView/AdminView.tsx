ecd8a7c8d6620c5a8118df0e76a5a320d104d364
Vlad Gafarov <vlad.gafarov02@gmail.com>
2022-12-01 21:42:45 +0500

    create admin view component

--- frontend/src/components/AdminView/AdminView.tsx
+++ frontend/src/components/AdminView/AdminView.tsx
@@ -1,7 +1,20 @@
+import AdminFeedback from '@components/AdminFeedback'
 import { FC } from 'react'
+import { EPages } from 'src/types/pages'
 
-const AdminView: FC = () => {
-	return <div>Admin View</div>
+const Components: { [key in EPages]: FC } = {
+	[EPages.Feedback]: AdminFeedback,
+	[EPages.Career]: () => <div>Career</div>,
+}
+
+interface Props {
+	page: EPages
+}
+
+const AdminView: FC<Props> = ({ page }) => {
+	const Component = Components[page]
+
+	return <Component />
 }
 
 export default AdminView
