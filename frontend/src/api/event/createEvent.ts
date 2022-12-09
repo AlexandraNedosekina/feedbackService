93dfef6fe1f587763742e0bfd621fc5fb8e5d228
Vlad Gafarov <vlad.gafarov02@gmail.com>
2022-12-09 22:03:17 +0500

    createEvent endpoint

---
+++ frontend/src/api/event/createEvent.ts
@@ -0,0 +1,53 @@
+import dayjs from 'dayjs'
+import api from '..'
+import { errorHandler } from '../errorHandler'
+
+async function createEvent(data: {
+	startDate: Date
+	endDate: Date
+	type: 'all' | 'one'
+	isTwoWay?: boolean
+	userId?: string
+}) {
+	let eventTypeUrl: 'all' | 'create_oneway' | 'create_twoway' = 'all'
+	let eventUrl = 'event/all'
+
+	if (dayjs(data.startDate).isAfter(data.endDate))
+		throw new Error('Start date cannot be after end date')
+
+	if (data.type === 'one') {
+		if (!data.userId) throw new Error('Missing userId')
+		if (!data.isTwoWay) throw new Error('Missing isTwoWay')
+
+		eventTypeUrl = data.isTwoWay ? 'create_twoway' : 'create_oneway'
+		eventUrl = `event/${eventTypeUrl}/${data.userId}`
+	}
+
+	if (data.type === 'all') {
+		if (data.userId)
+			throw new Error('Unexpected, userId is not needed for all events')
+		if (data.isTwoWay)
+			throw new Error('Unexpected, isTwoWay is not needed for all events')
+	}
+
+	try {
+		const res = await api.post(
+			eventUrl,
+			{
+				date_start: data.startDate,
+				date_stop: data.endDate,
+			},
+			{
+				headers: {
+					'Content-Type': 'application/json',
+				},
+			}
+		)
+
+		return res.data
+	} catch (error: any) {
+		throw new Error(errorHandler(error))
+	}
+}
+
+export default createEvent
