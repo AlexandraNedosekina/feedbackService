d9954d8654e4e565727638d0163de88b6a717e2a
Vlad Gafarov <vlad.gafarov02@gmail.com>
2022-12-12 21:18:52 +0500

    update

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -39,7 +39,6 @@ const queryClient = new QueryClient({
 	},
 })
 
-// eslint-disable-next-line @typescript-eslint/ban-types
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
 	getLayout?: (page: ReactElement) => ReactNode
 }
