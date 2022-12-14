43960c0662c49e16232e714ec1f0d47ca7de011f
Vlad Gafarov <vlad.gafarov02@gmail.com>
2022-12-14 18:25:21 +0500

    admin career page

--- frontend/src/components/AdminCareer/AdminCareer.tsx
+++ frontend/src/components/AdminCareer/AdminCareer.tsx
@@ -1,12 +1,127 @@
-import { UserSearchSelect } from '@components/UserSearchSelect'
-import { Container, Title } from '@mantine/core'
+import Icon from '@components/Icon'
+import {
+	ActionIcon,
+	Box,
+	Container,
+	Flex,
+	Input,
+	Table,
+	Title,
+} from '@mantine/core'
+import { useDebouncedValue } from '@mantine/hooks'
+import { useQuery } from '@tanstack/react-query'
+import {
+	createColumnHelper,
+	flexRender,
+	getCoreRowModel,
+	useReactTable,
+} from '@tanstack/react-table'
+import { useEffect, useState, useMemo } from 'react'
+import { getAllUsers, QueryKeys, searchUserByFullname } from 'src/api'
+import { User } from 'src/api/generatedTypes'
+import tableStyles from 'src/styles/table.module.sass'
+
+const columnHelper = createColumnHelper<User>()
+
+const columns = [
+	columnHelper.accessor('full_name', {
+		header: 'Сотрудник',
+	}),
+	columnHelper.accessor('job_title', {
+		header: 'Должность',
+		cell({ getValue }) {
+			return getValue() || 'Не указана'
+		},
+	}),
+	columnHelper.display({
+		id: 'edit',
+		cell: ({ row }) => (
+			<Flex justify={'end'}>
+				<ActionIcon color="brand">
+					<Icon icon="edit" />
+				</ActionIcon>
+			</Flex>
+		),
+	}),
+]
 
 const AdminCareer = () => {
+	const [searchValue, setSearchValue] = useState<string>('')
+	const [debounced] = useDebouncedValue(searchValue, 300)
+	const { data: users, isLoading } = useQuery({
+		queryKey: [QueryKeys.USERS],
+		queryFn: () => getAllUsers(),
+	})
+	const {
+		data: searchUsers,
+		isLoading: searchUsersLoading,
+		refetch,
+	} = useQuery({
+		queryKey: [QueryKeys.SEARCH_USERS],
+		queryFn: () => searchUserByFullname(debounced),
+		enabled: !!debounced,
+	})
+	const searchUsersParsed = useMemo<User[]>(() => {
+		return searchUsers?.map(user => user.original) || []
+	}, [searchUsers])
+
+	const table = useReactTable({
+		data: debounced ? searchUsersParsed : users || [],
+		columns,
+		getCoreRowModel: getCoreRowModel(),
+	})
+
+	useEffect(() => {
+		if (debounced) {
+			refetch()
+		}
+	}, [debounced, refetch])
+
 	return (
 		<Container pt="lg">
 			<Title order={2}>Карьерный рост</Title>
 
-			<UserSearchSelect />
+			<Box maw={400} mt="lg">
+				<Input
+					value={searchValue}
+					onChange={e => setSearchValue(e.target.value)}
+					icon={<Icon icon="search" />}
+					placeholder="Поиск"
+				/>
+			</Box>
+
+			<Table className={tableStyles.table} mt="lg">
+				<thead>
+					{table.getHeaderGroups().map(headerGroup => (
+						<tr key={headerGroup.id}>
+							{headerGroup.headers.map(header => (
+								<th key={header.id}>
+									{header.isPlaceholder
+										? null
+										: flexRender(
+												header.column.columnDef.header,
+												header.getContext()
+										  )}
+								</th>
+							))}
+						</tr>
+					))}
+				</thead>
+				<tbody>
+					{table.getRowModel().rows.map(row => (
+						<tr key={row.id}>
+							{row.getVisibleCells().map(cell => (
+								<td key={cell.id}>
+									{flexRender(
+										cell.column.columnDef.cell,
+										cell.getContext()
+									)}
+								</td>
+							))}
+						</tr>
+					))}
+				</tbody>
+			</Table>
 		</Container>
 	)
 }
