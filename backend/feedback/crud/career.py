305e5f2d2a189f61a370a583f1c307e82aef6075
vazik <70693527+vazik66@users.noreply.github.com>
2022-12-21 16:35:55 +0500

    added more field to feedback statistics

--- backend/feedback/crud/career.py
+++ backend/feedback/crud/career.py
@@ -15,10 +15,10 @@ class CRUDCareer(
             career_param = models.CareerParam(**param.dict())
             career_params.append(career_param)
 
-        obj_in: dict = obj_in.dict()
-        obj_in.pop("params")
+        obj_in_without_params: dict = obj_in.dict()
+        obj_in_without_params.pop("params")
 
-        db_obj = models.CareerTrack(**obj_in, params=career_params)
+        db_obj = models.CareerTrack(**obj_in_without_params, params=career_params)
         db.add(db_obj)
         db.commit()
         db.refresh(db_obj)
