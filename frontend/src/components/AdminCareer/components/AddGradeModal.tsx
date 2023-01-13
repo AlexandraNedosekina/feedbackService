498f98992dff37e53d326c9491a6fed88929e331
Vlad Gafarov <vlad.gafarov02@gmail.com>
2023-01-13 23:44:58 +0500

    refactor add grade modal with final-form

--- frontend/src/components/AdminCareer/components/AddGradeModal.tsx
+++ frontend/src/components/AdminCareer/components/AddGradeModal.tsx
@@ -1,18 +1,31 @@
-import { Modal, Stack, Title } from '@mantine/core'
+import { FormInput, required } from '@components/form'
+import { Button, Flex, Modal, Stack, Title } from '@mantine/core'
 import { FC } from 'react'
+import { Form, FormSpy } from 'react-final-form'
 import { useAddCareerGrade } from 'src/stores'
-import SalaryInput from './SalaryInput'
-import SubmitButton from './SubmitButton'
-import Tasks from './Tasks'
-import TitleInput from './TitleInput'
 
 interface Props {
 	isOpen: boolean
 	onClose: () => void
 }
 
+interface FormValues {
+	title: string
+	salary: string
+}
+
 const AddGradeModal: FC<Props> = ({ isOpen, onClose }) => {
 	const isEdit = useAddCareerGrade(state => state.isEdit)
+
+	// @ts-expect-error implicity any
+	function onSubmit(data) {
+		console.log(data)
+	}
+
+	const initialValues: FormValues = {
+		title: '',
+		salary: '',
+	}
 
 	return (
 		<Modal
@@ -25,13 +38,73 @@ const AddGradeModal: FC<Props> = ({ isOpen, onClose }) => {
 			}
 			size="lg"
 		>
-			<Stack>
-				<TitleInput />
-				<SalaryInput />
-				<Tasks title="Что нужно изучить" type="toLearn" />
-				<Tasks title="Что нужно сделать" type="toComplete" />
-				<SubmitButton onClose={onClose} />
-			</Stack>
+			<Form
+				onSubmit={onSubmit}
+				initialValues={initialValues}
+				subscription={{
+					submitting: true,
+					pristine: true,
+				}}
+			>
+				{props => {
+					return (
+						<form onSubmit={props.handleSubmit}>
+							<Stack>
+								<FormInput
+									fieldProps={{
+										name: 'title',
+										validate: required,
+									}}
+									inputProps={{
+										label: 'Название',
+										autoComplete: 'off',
+										withAsterisk: true,
+										required: true,
+									}}
+								/>
+								<FormInput
+									fieldProps={{
+										name: 'salary',
+									}}
+									inputProps={{
+										label: 'Зарплата',
+										type: 'number',
+										autoComplete: 'off',
+									}}
+								/>
+								{/* <SalaryInput />
+        <Tasks title="Что нужно изучить" type="toLearn" />
+    <Tasks title="Что нужно сделать" type="toComplete" /> */}
+								{/* <SubmitButton onClose={onClose} /> */}
+								<FormSpy>
+									{({
+										hasValidationErrors,
+										submitting,
+										pristine,
+										invalid,
+									}) => {
+										return (
+											<Flex justify={'end'}>
+												<Button
+													type="submit"
+													disabled={
+														submitting ||
+														pristine ||
+														invalid ||
+														hasValidationErrors
+													}
+												>
+													Submit
+												</Button>
+											</Flex>
+										)
+									}}
+								</FormSpy>
+							</Stack>
+						</form>
+					)
+				}}
+			</Form>
 		</Modal>
 	)
 }
