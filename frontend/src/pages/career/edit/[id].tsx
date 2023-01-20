dcfa36346a0ad707cec48a40055103bdcbd6082d
Vlad Gafarov <vlad.gafarov02@gmail.com>
2023-01-20 22:16:08 +0500

    redirect from admin career whet !isEdit

--- frontend/src/pages/career/edit/[id].tsx
+++ frontend/src/pages/career/edit/[id].tsx
@@ -1,9 +1,25 @@
 import { EditCareer } from '@components/AdminCareer/components'
 import Head from 'next/head'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
 import { BaseLayout } from 'src/layouts'
 import { NextPageWithLayout } from 'src/pages/_app'
+import { ERoles } from 'src/types/roles'
+import { useBaseLayoutContext } from 'src/utils/useBaseLayoutContext'
+import { useUser } from 'src/utils/useUser'
 
 const EditCareerPage: NextPageWithLayout = () => {
+	const { user } = useUser()
+	const { isEdit } = useBaseLayoutContext()
+	const router = useRouter()
+
+	useEffect(() => {
+		if (!user?.roles.includes(ERoles.admin) || !isEdit) {
+			router.push('/career')
+		}
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [isEdit])
+
 	return (
 		<>
 			<Head>
