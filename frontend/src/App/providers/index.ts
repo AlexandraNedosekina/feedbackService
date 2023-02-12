3ce8bea3767f516914e17f957bc8e3f7fb9eed92
Vlad Gafarov <vlad.gafarov02@gmail.com>
2023-02-12 11:41:18 +0500

    rename App

--- frontend/src/App/providers/index.ts
+++
@@ -1,6 +0,0 @@
-import compose from 'compose-function'
-import { withMantine } from './withMantine'
-import { withQueryClient } from './withQueryClient'
-
-export const withProviders = compose(withMantine, withQueryClient)
-export * from './withQueryClient'
