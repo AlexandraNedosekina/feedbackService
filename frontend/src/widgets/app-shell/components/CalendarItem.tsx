6845143e05a4f4b3a06086403c39afb6cfba79b5
Vlad Gafarov <vlad.gafarov02@gmail.com>
2023-03-07 10:29:16 +0500

    add notify in calendar nav item

--- frontend/src/widgets/app-shell/components/CalendarItem.tsx
+++ frontend/src/widgets/app-shell/components/CalendarItem.tsx
@@ -1,8 +1,34 @@
+import { useQuery } from '@tanstack/react-query'
+import dayjs from 'dayjs'
+import { useUser } from 'entities/user'
 import { useRouter } from 'next/router'
+import { useMemo } from 'react'
+import { myCalendar, QueryKeys } from 'shared/api'
+import { CalendarEventStatus, CalendarFormat } from 'shared/api/generatedTypes'
 import NavItem from './NavItem'
 
 export default () => {
 	const router = useRouter()
+	const { user } = useUser()
+
+	const { data } = useQuery({
+		queryKey: [QueryKeys.CALENDAR],
+		queryFn: () =>
+			myCalendar(
+				dayjs(Date.now()).format('YYYY-MM-DD'),
+				CalendarFormat.Month
+			),
+		select: data =>
+			data.map(item => ({
+				status: item.status,
+				userId: item.user_id,
+			})),
+	})
+	const notify = useMemo(() => {
+		return data?.some(
+			i => i.status === CalendarEventStatus.Pending && i.userId === user.id
+		)
+	}, [data, user.id])
 
 	return (
 		<NavItem
@@ -10,6 +36,7 @@ export default () => {
 			href={'/calendar'}
 			text={'Календарь встреч'}
 			active={router.pathname.split('/')[1] === 'calendar'}
+			notify={notify}
 		/>
 	)
 }
