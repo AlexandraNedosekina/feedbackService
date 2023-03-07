cf6b35180db12e0c19c2893a298b384695cd1690
Vlad Gafarov <vlad.gafarov02@gmail.com>
2023-03-07 16:01:08 +0500

    reject calendar event

--- frontend/src/features/calendar-accept-events/components/Reject.tsx
+++ frontend/src/features/calendar-accept-events/components/Reject.tsx
@@ -1,8 +1,9 @@
-import { ActionIcon } from '@mantine/core'
+import { Button, Flex, Modal, Text, TextInput } from '@mantine/core'
+import { useDisclosure } from '@mantine/hooks'
 import { showNotification } from '@mantine/notifications'
 import { useMutation, useQueryClient } from '@tanstack/react-query'
-import { acceptCalendarEvent, QueryKeys, rejectCalendarEvent } from 'shared/api'
-import { Icon } from 'shared/ui'
+import { useState } from 'react'
+import { QueryKeys, rejectCalendarEvent } from 'shared/api'
 import ActionTemplate from './ActionTemplate'
 
 interface IProps {
@@ -10,14 +11,18 @@ interface IProps {
 }
 
 export default function ({ eventId }: IProps) {
+	const [isOpen, modalHandlers] = useDisclosure(false)
+	const [rejectionCause, setRejectionCause] = useState<string>()
+
 	const queryClient = useQueryClient()
 	const { mutate, isLoading } = useMutation({
 		mutationFn: () =>
-			rejectCalendarEvent(eventId, { rejection_cause: undefined }),
+			rejectCalendarEvent(eventId, { rejection_cause: rejectionCause }),
 		onSuccess: () => {
+			modalHandlers.close()
 			showNotification({
 				title: 'Успешно',
-				message: 'Встреча подтверждена',
+				message: 'Встреча отклонена',
 				color: 'green',
 			})
 			queryClient.invalidateQueries([QueryKeys.CALENDAR])
@@ -25,11 +30,37 @@ export default function ({ eventId }: IProps) {
 	})
 
 	return (
-		<ActionTemplate
-			icon="close"
-			color="red"
-			loading={isLoading}
-			tooltipText="Отменить встречу"
-		/>
+		<>
+			<ActionTemplate
+				onClick={modalHandlers.open}
+				icon="close"
+				color="red"
+				tooltipText="Отменить встречу"
+			/>
+			<Modal
+				opened={isOpen}
+				onClose={modalHandlers.close}
+				title="Вы действительно хотите отклонить встречу?"
+			>
+				<TextInput
+					value={rejectionCause}
+					onChange={e => setRejectionCause(e.currentTarget.value)}
+					label={
+						<Text>
+							Причина:{' '}
+							<Text color="gray.6" weight="normal" span>
+								(необязательно)
+							</Text>
+						</Text>
+					}
+				/>
+
+				<Flex mt="lg" justify={'end'} gap="md">
+					<Button color="red" loading={isLoading} onClick={() => mutate()}>
+						Отклонить
+					</Button>
+				</Flex>
+			</Modal>
+		</>
 	)
 }
