591a02d7ddb15ccbb73c3f9a9f63811f3f00ab8f
Vlad Gafarov <vlad.gafarov02@gmail.com>
2023-03-27 22:29:41 +0500

    refactoring event creation add form & its types

--- frontend/src/features/event-create/components/index.tsx
+++ frontend/src/features/event-create/components/index.tsx
@@ -1,3 +1,6 @@
+import dayjs from 'dayjs'
+import { Form } from 'react-final-form'
+import { IFormValues } from '../types'
 import Buttons from './Buttons'
 import SelectType from './SelectType'
 import TimePicker from './TimePicker'
@@ -7,11 +10,27 @@ interface IProps {
 }
 
 export default ({ onCancel }: IProps) => {
+	function handleSubmit(values: IFormValues) {
+		throw new Error('Function not implemented.')
+	}
+
 	return (
-		<>
-			<SelectType />
-			<TimePicker />
-			<Buttons onCancel={onCancel} />
-		</>
+		<Form<IFormValues>
+			onSubmit={handleSubmit}
+			subscription={{ pristine: true, values: true }}
+			initialValues={{
+				type: 'all',
+				startTime: dayjs().toISOString(),
+				endTime: dayjs().add(2, 'weeks').toISOString(),
+			}}
+		>
+			{() => (
+				<>
+					<SelectType />
+					<TimePicker />
+					<Buttons onCancel={onCancel} />
+				</>
+			)}
+		</Form>
 	)
 }
---
+++ frontend/src/features/event-create/types.ts
@@ -0,0 +1,7 @@
+export interface IFormValues {
+	type: 'one' | 'all'
+	startTime: string
+	endTime: string
+	twoWay?: boolean
+	userId?: string
+}
