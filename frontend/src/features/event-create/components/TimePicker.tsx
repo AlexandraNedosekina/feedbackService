8da9bb114f3869add144883b00769afd53fca530
Vlad Gafarov <vlad.gafarov02@gmail.com>
2023-03-31 19:02:18 +0500

    event create refactoring

--- frontend/src/features/event-create/components/TimePicker.tsx
+++ frontend/src/features/event-create/components/TimePicker.tsx
@@ -1,108 +1,21 @@
 import { Box, Group, Text } from '@mantine/core'
-import { DatePickerInput, TimeInput } from '@mantine/dates'
-import dayjs from 'dayjs'
-import shallow from 'zustand/shallow'
-import { useCreateEventStore } from '../model'
+import { FormDatePickerInput, FormTimeInput, required } from 'shared/ui'
 
 const TimePicker = () => {
-	const {
-		endTime,
-		startTime,
-		endDate,
-		startDate,
-		getIsEndDateAfter,
-		getIsEndTimeAfter,
-		getIsSameDates,
-		getIsStartTimeAfterNow,
-		getIsStartDateAfterNow,
-		getIsStartDateSame,
-	} = useCreateEventStore(
-		state => ({
-			startTime: state.startTime,
-			endTime: state.endTime,
-			startDate: state.startDate,
-			endDate: state.endDate,
-			getIsEndDateAfter: state.getIsEndDateAfter,
-			getIsEndTimeAfter: state.getIsEndTimeAfter,
-			getIsSameDates: state.getIsSameDates,
-			getIsStartTimeAfterNow: state.getIsStartTimeAfterNow,
-			getIsStartDateAfterNow: state.getIsStartDateAfterNow,
-			getIsStartDateSame: state.getIsStartDateSame,
-		}),
-		shallow
-	)
-	const update = useCreateEventStore(state => state.update)
-
 	return (
 		<>
 			<Box mt="md">
 				<Text>Начало</Text>
 				<Group align="start">
-					<TimeInput
-						defaultValue={dayjs(startTime).format('hh:mm')}
-						onChange={({ target: { value } }) => {
-							const [hours, minutes] = value.split(':')
-							update({
-								startTime: dayjs()
-									.set('hours', +hours)
-									.set('minutes', +minutes)
-									.toDate(),
-							})
-						}}
-						maw={'min-content'}
-						error={
-							getIsStartDateSame() && !getIsStartTimeAfterNow()
-								? 'Время начала должно быть позже текущего'
-								: ''
-						}
-					/>
-					<DatePickerInput
-						placeholder="Выберите дату"
-						defaultValue={startDate}
-						dropdownType="modal"
-						modalProps={{ zIndex: 500 }}
-						onChange={value => update({ startDate: value })}
-						error={
-							!getIsStartDateAfterNow() && !getIsStartDateSame()
-								? 'Дата начала должна быть позже текущей'
-								: ''
-						}
-					/>
+					<FormTimeInput name="startTime" validate={required} />
+					<FormDatePickerInput name="startDate" validate={required} />
 				</Group>
 			</Box>
 			<Box mt="md">
 				<Text>Окончание</Text>
 				<Group align="start">
-					<TimeInput
-						defaultValue={dayjs(endTime).format('hh:mm')}
-						onChange={({ target: { value } }) => {
-							const [hours, minutes] = value.split(':')
-							update({
-								endTime: dayjs()
-									.set('hours', +hours)
-									.set('minutes', +minutes)
-									.toDate(),
-							})
-						}}
-						maw={'min-content'}
-						error={
-							!getIsEndTimeAfter() && getIsSameDates()
-								? 'Время окончания должно быть позже времени начала'
-								: ''
-						}
-					/>
-					<DatePickerInput
-						placeholder="Выберите дату"
-						defaultValue={endDate}
-						dropdownType="modal"
-						modalProps={{ zIndex: 500 }}
-						onChange={value => update({ endDate: value })}
-						error={
-							!getIsEndDateAfter() && !getIsSameDates()
-								? 'Дата окончания должна быть позже или равна дате начала'
-								: ''
-						}
-					/>
+					<FormTimeInput name="endTime" validate={required} />
+					<FormDatePickerInput name="endDate" validate={required} />
 				</Group>
 			</Box>
 		</>
--- frontend/src/features/event-create/components/index.tsx
+++ frontend/src/features/event-create/components/index.tsx
@@ -1,5 +1,8 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query'
 import dayjs from 'dayjs'
+import { FORM_ERROR } from 'final-form'
 import { Form, FormSpy } from 'react-final-form'
+import { createEvent, QueryKeys } from 'shared/api'
 import { FormCondition } from 'shared/ui'
 import { IFormValues } from '../types'
 import Buttons from './Buttons'
@@ -12,21 +15,76 @@ interface IProps {
 }
 
 export default ({ onCancel }: IProps) => {
-	function handleSubmit(values: IFormValues) {
-		throw new Error('Function not implemented.')
+	const queryClient = useQueryClient()
+
+	const { mutateAsync } = useMutation({
+		mutationFn: (data: {
+			startDate: Date
+			endDate: Date
+			type: 'all' | 'one'
+			isTwoWay?: boolean
+			userId?: string
+		}) => createEvent(data),
+		onSuccess: () => {
+			queryClient.invalidateQueries([QueryKeys.EVENTS])
+			onCancel?.()
+		},
+	})
+
+	async function handleSubmit(values: IFormValues) {
+		const [startHours, startMinutes] = values.startTime.split(':')
+		const [endHours, endMinutes] = values.endTime.split(':')
+		const start = dayjs(values.startDate)
+			.set('hours', +startHours)
+			.set('minutes', +startMinutes)
+			.toDate()
+		const end = dayjs(values.endDate)
+			.set('hours', +endHours)
+			.set('minutes', +endMinutes)
+			.toDate()
+
+		return { [FORM_ERROR]: 'something went wrong' }
+
+		//return mutateAsync({
+		//type: values.type,
+		//startDate: start,
+		//endDate: end,
+		//userId: values.userId,
+		//isTwoWay: values.twoWay,
+		//}).catch(() => ({ [FORM_ERROR]: 'something went wrong' }))
 	}
 
 	return (
 		<Form<IFormValues>
 			onSubmit={handleSubmit}
 			subscription={{ pristine: true, values: true }}
+			keepDirtyOnReinitialize
 			initialValues={{
 				type: 'all',
-				startTime: dayjs().toISOString(),
-				endTime: dayjs().add(2, 'weeks').toISOString(),
+				startTime: dayjs().add(1, 'hour').set('minutes', 0).format('HH:mm'),
+				//@ts-expect-error initial value must be type of Date
+				startDate: dayjs().startOf('day').toDate(),
+				endTime: dayjs().startOf('day').format('HH:mm'),
+				//@ts-expect-error initial value must be type of Date
+				endDate: dayjs().add(2, 'weeks').startOf('day').toDate(),
+				twoWay: false,
+			}}
+			validate={values => {
+				const errors: Partial<Record<keyof IFormValues, string>> = {}
+
+				if (
+					dayjs(values.endDate).isBefore(values.startDate) ||
+					(dayjs(values.endDate).isSame(values.startDate) &&
+						+values.startTime.replace(':', '') >
+						+values.endTime.replace(':', ''))
+				) {
+					errors.endDate = 'Окончание не может быть раньше начала'
+				}
+
+				return errors
 			}}
 		>
-			{() => (
+			{({ handleSubmit, submitError }) => (
 				<>
 					<FormSpy>
 						{({ values }) => <pre>{JSON.stringify(values, null, 2)}</pre>}
@@ -36,7 +94,8 @@ export default ({ onCancel }: IProps) => {
 						<UserSelect />
 					</FormCondition>
 					<TimePicker />
-					<Buttons onCancel={onCancel} />
+					{submitError && <p>{submitError}</p>}
+					<Buttons handleSubmit={handleSubmit} onCancel={onCancel} />
 				</>
 			)}
 		</Form>
