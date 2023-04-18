99b3db1eb794654059a81436eb08ee3fe8abec58
Alexandra Nedosekina <volk.sash10@gmail.com>
2023-04-18 14:59:22 +0300

    Добавлена подпись когда табица пустая

--- frontend/src/shared/ui/Table/components/Table.tsx
+++ frontend/src/shared/ui/Table/components/Table.tsx
@@ -1,4 +1,4 @@
-import { Table as TableMantine } from '@mantine/core'
+import { Table as TableMantine, Text } from '@mantine/core'
 import { flexRender, Table as ITable } from '@tanstack/react-table'
 import tableStyles from './styles.module.sass'
 
@@ -8,38 +8,43 @@ interface IProps {
 
 const Table = ({ table }: IProps) => {
 	return (
-		<TableMantine className={tableStyles.table}>
-			<thead>
-				{table.getHeaderGroups().map(headerGroup => (
-					<tr key={headerGroup.id}>
-						{headerGroup.headers.map(header => (
-							<th key={header.id}>
-								{header.isPlaceholder
-									? null
-									: flexRender(
-											header.column.columnDef.header,
-											header.getContext()
-									  )}
-							</th>
-						))}
-					</tr>
-				))}
-			</thead>
-			<tbody>
-				{table.getRowModel().rows.map(row => (
-					<tr key={row.id}>
-						{row.getVisibleCells().map(cell => (
-							<td key={cell.id}>
-								{flexRender(
-									cell.column.columnDef.cell,
-									cell.getContext()
-								)}
-							</td>
-						))}
-					</tr>
-				))}
-			</tbody>
-		</TableMantine>
+		<>
+			<TableMantine className={tableStyles.table}>
+				<thead>
+					{table.getHeaderGroups().map(headerGroup => (
+						<tr key={headerGroup.id}>
+							{headerGroup.headers.map(header => (
+								<th key={header.id}>
+									{header.isPlaceholder
+										? null
+										: flexRender(
+												header.column.columnDef.header,
+												header.getContext()
+										  )}
+								</th>
+							))}
+						</tr>
+					))}
+				</thead>
+				<tbody>
+					{table.getRowModel().rows.map(row => (
+						<tr key={row.id}>
+							{row.getVisibleCells().map(cell => (
+								<td key={cell.id}>
+									{flexRender(
+										cell.column.columnDef.cell,
+										cell.getContext()
+									)}
+								</td>
+							))}
+						</tr>
+					))}
+					{table?.getRowModel().rows.length === 0 && (
+						<Text ta="right">Записей нет</Text>
+					)}
+				</tbody>
+			</TableMantine>
+		</>
 	)
 }
 
